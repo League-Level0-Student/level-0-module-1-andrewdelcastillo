af28c3aba26b9fcc313b848c6bb6b099f35cbedb
codeclub <codeclub@smms-rm23-20.smusd.local>
2018-02-07 14:58:15 -0800

    The Riddler xgeeks in progress

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,17 +10,34 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int x = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+String input = JOptionPane.showInputDialog(null,"What can travel while staying in a corner?");
+	
+// 4. If they got the answer right, pop up "correct!" and increase the score by one
+if(input.equals("a stamp")) {
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+JOptionPane.showMessageDialog(null,"correct");
+}else {
+	
 		// 6. Add some more riddles
-
+JOptionPane.showMessageDialog(null, "incorrect");
 		// 2. Make a pop up to show the score.
-		
+String input2 = JOptionPane.showInputDialog(null,"what can run the fastest?");
+
+//4. If they got the answer right, pop up "correct!" and increase the score by one
+if(input2.equals("a cheetah")) {
+		// 5. Otherwise, say "wrong" and tell them the answer
+JOptionPane.showMessageDialog(null,"correct");
+}else {
+	
+		// 6. Add some more riddles
+JOptionPane.showMessageDialog(null, "incorrect");
 	}
 }
+}
 
+	
+	
+	
+}
